31c8a69e61c3f2b4fa6f89534f26f65427dc0cb8
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-03-08 11:00:52 +0100

    add publish output

--- core/src/actions.rs
+++ core/src/actions.rs
@@ -1,15 +1,19 @@
+use std::io::{self, Write};
 use std::path::PathBuf;
 
-use anyhow::Result;
+use anyhow::{bail, Ok, Result};
+use colored::Colorize;
 use move_package::BuildConfig;
 use sui::client_commands::{SuiClientCommandResult, SuiClientCommands, WalletContext};
-use sui_json_rpc_types::SuiTransactionResponse;
+use sui_json_rpc_types::{SuiEvent, SuiTransactionResponse};
 
 pub async fn publish_package(
     package_path: PathBuf,
     context: &mut WalletContext,
     build_config: BuildConfig,
 ) -> Result<SuiTransactionResponse> {
+    let mut w = io::stdout();
+
     let command = SuiClientCommands::Publish {
         package_path,
         build_config,
@@ -21,8 +25,76 @@ pub async fn publish_package(
 
     let result = command.execute(context).await?;
     if let SuiClientCommandResult::Publish(response) = result {
+        writeln!(
+            &mut w,
+            "\nTransaction Digest: {}",
+            response.certificate.transaction_digest
+        )
+        .unwrap();
+
+        response.effects.events.iter().for_each(|e| {
+            if let SuiEvent::Publish {
+                sender: _,
+                package_id,
+                version: _,
+                digest: _,
+            } = e
+            {
+                writeln!(&mut w, "Package ID: {}", package_id).unwrap()
+            }
+        });
+
+        if !response.effects.created.is_empty() {
+            writeln!(&mut w, "\nObjects Created")?;
+
+            response.effects.created.iter().for_each(|object| {
+                writeln!(
+                    &mut w,
+                    "  - Object ID: {}\n    Owner: {}\n    Digest: {}\n    Version: {}",
+                    object.reference.object_id,
+                    object.owner,
+                    object.reference.digest,
+                    &object.reference.version.value()
+                )
+                .unwrap();
+            });
+        }
+
+        if !response.effects.mutated.is_empty() {
+            writeln!(&mut w, "\nObjects Mutated")?;
+
+            response.effects.mutated.iter().for_each(|object| {
+                writeln!(
+                    &mut w,
+                    "  - Object ID: {}\n    Owner: {}\n    Digest: {}\n    Version: {}",
+                    object.reference.object_id,
+                    object.owner,
+                    object.reference.digest,
+                    &object.reference.version.value()
+                )
+                .unwrap()
+            });
+        }
+
+        if !response.effects.deleted.is_empty() {
+            writeln!(&mut w, "\nObjects Deleted")?;
+
+            response.effects.deleted.iter().for_each(|object| {
+                writeln!(
+                    &mut w,
+                    "  - Object ID: {}\n    Digest: {}\n    Version: {}",
+                    object.object_id,
+                    object.digest,
+                    &object.version.value()
+                )
+                .unwrap()
+            });
+        }
+
+        writeln!(&mut w, "{}", "\nPUBLISHED SUCCESSFULLY 🥳".green().bold())?;
+
         Ok(response)
     } else {
-        panic!("")
+        bail!("")
     }
 }
