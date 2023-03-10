618451ee61f4f5ea012752bb15e999a1fddad7df
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-03-10 16:21:15 +0100

    update template

--- cli/src/template.rs
+++ cli/src/template.rs
@@ -1,10 +1,10 @@
 pub const CARTON_MANIFEST_TEMPLATE: &str = "
 [provider]
-address = \"0x0\"
+address = \"0000000000000000000000000000000000000000\"
 env = \"devnet\"
 config = \"~/.sui/sui_config/client.yaml\"
 
 [envs]
-devnet = { url = \"https://fullnode.devnet.sui.io/\" }
-testnet = { url = \"https://fullnode.testnet.sui.io/\" }
+devnet = { url = \"https://fullnode.devnet.sui.io:443/\" }
+testnet = { url = \"https://fullnode.testnet.sui.io:443/\" }
 ";
