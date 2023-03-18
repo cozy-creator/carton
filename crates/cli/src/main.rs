550fa194dbec354a3646b2ac10e3b0c66a3de87c
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-03-18 12:08:38 +0100

    update error print

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -5,7 +5,7 @@ use colored::Colorize;
 #[tokio::main]
 async fn main() {
     if let Err(e) = carton::run_cli().await {
-        eprintln!("{}", e.to_string().bold().red());
+        eprintln!("Error: {}", e.to_string().bold().red());
         process::exit(1);
     }
 }
